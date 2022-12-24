64e50faa17d29f1ddbfbda9a2be56744750acff9
dimakovalev25 <d.nozdryn@gmail.com>
2022-12-24 21:01:23 +0300

    bindActionCreator

--- src/App.js
+++ src/App.js
@@ -1,54 +1,37 @@
 import './App.css';
 import {createStore} from "redux";
 
-const initialState = {value: 0};
+import reducer from "./reducer";
+import {inc, dec, rnd} from "./actions";
 
-const reducer = (state = initialState, action) => {
-    switch (action.type) {
-        case 'INC':
-            return {
-                ...state,
-                value: state.value + 1
-            };
-        case 'DEC':
-            return {
-                ...state,
-                value: state.value -1
-            };
-        case 'RND':
-            return  {
-                ...state,
-                value: state.value * action.payload
-            };
-        default:
-            return state;
-    }
-}
+
+
 
 const store = createStore(reducer);
+const {dispatch, subscribe, getState} = store;
 
 const update = () => {
-    // console.log(store.getState())
+    console.log(getState())
     document.getElementById('counter').textContent = store.getState().value;
 }
 
-store.subscribe(update);
+subscribe(update);
 
-const inc = () => ({type: 'INC'})
-const dec = () => ({type: 'DEC'})
-const rnd = (value) => ({type: 'RND', payload: value})
+const bindActionCreator = (creator, dispatch) => (...args) => {
+    dispatch(creator(...args));
+}
 
-document.getElementById('inc').addEventListener('click', () => {
-    store.dispatch(inc());
-});
+const incDispatch = bindActionCreator(inc, dispatch);
+const decDispatch = bindActionCreator(dec, dispatch);
+const rndDispatch = bindActionCreator(rnd, dispatch);
 
-document.getElementById('dec').addEventListener('click', () => {
-    store.dispatch(dec());
-});
+document.getElementById('inc').addEventListener('click', incDispatch);
+
+document.getElementById('dec').addEventListener('click', decDispatch);
 
 document.getElementById('rnd').addEventListener('click', () => {
     const value = Math.floor(Math.random() * (5 - 1)) + 1;
-    store.dispatch(rnd(value));
+    rndDispatch(value);
 });
 
 
--- src/index.js
+++ src/index.js
@@ -3,38 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {createStore} from "redux";
-
-
-const ititialState = 0;
-
-const reducer = (state = 15, action) => {
-    switch (action.type) {
-        case 'INC':
-            return state + 1;
-        case 'DEC':
-            return state - 1;
-        default:
-            return state;
-    }
-}
-
-// let state = reducer(undefined, {type: 'INC'});
-// state = reducer(state, {type: 'INC'});
-// state = reducer(state, {type: 'INC'});
-// console.log(state);
-
-const store = createStore(reducer);
-store.subscribe(() => {
-    console.log(store.getState());
-
-});
-
-store.dispatch({type: 'INC'});
-store.dispatch({type: 'INC'});
-store.dispatch({type: 'INC'});
-
-
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
